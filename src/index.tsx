63c335f9b2ca1c053d68dfc64175b70525c0c817
Felipe Sakuma <fycorsak@gmail.com>
2022-05-16 19:39:20 -0300

    chore: add roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import './styles/reset.css';
 import './styles/global.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
